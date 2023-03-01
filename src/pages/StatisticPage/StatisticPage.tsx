4acf29ba14602965d9f1c4d9b34bc8bee543880f
Daniel <usechedaniel88@gmail.com>
2023-03-01 22:40:35 +0100

    statistic-page al 40%

--- src/pages/StatisticPage/StatisticPage.tsx
+++ src/pages/StatisticPage/StatisticPage.tsx
@@ -1,12 +1,21 @@
+import { Container } from "../../Global.style"
+import imageSalad from '../../assets/images/salad.png';
+import imageBurger from '../../assets/images/burger.png';
+import iconoStatistic from '../../assets/icons/statistic-pie-chart-red.png';
+import { DivStatistic, StyleIconoStatistic, StyleImageSalad } from "./statisticPage.style";
 
 type Props = {}
 
 const StatisticPage = (props: Props) => {
   return (
-    <div>
-        <h1>Statistic Page</h1>
-        <h2>"Contenido"</h2>
-    </div>
+    <Container>
+        <DivStatistic>
+        <StyleIconoStatistic src={iconoStatistic} alt="icono statistic" />
+        <h1>Statistic</h1>
+        </DivStatistic>
+        <StyleImageSalad src={imageSalad} alt="image salad" />
+        <StyleImageSalad src={imageBurger} alt="image burger" />
+    </Container>
   )
 }
 
--- src/pages/StatisticPage/statisticPage.style.tsx
+++ src/pages/StatisticPage/statisticPage.style.tsx
@@ -1,1 +1,18 @@
-export {}
+import styled from "styled-components";
+
+
+export const StyleImageSalad = styled.img`
+width: 100%;
+margin: 40px;
+`
+
+export const StyleIconoStatistic = styled.img`
+`
+
+export const DivStatistic = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+width: 70%;
+`
+    
